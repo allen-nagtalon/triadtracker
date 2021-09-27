7a0aeb488dde7791cecc4c08fb6184c79cd02c81
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-09-27 12:43:55 -0700

    Ran migrations and imported api card data

--- triadtrackerapp/views.py
+++ triadtrackerapp/views.py
@@ -1,5 +1,6 @@
 import http.client
 import json
+from triadtrackerapp.models import TriadCard
 from django.shortcuts import render
 
 def populateCards(request):
@@ -7,8 +8,26 @@ def populateCards(request):
     payload = ''
     headers = {}
     conn.request("GET", "/api/cards", payload, headers)
+
     res = conn.getresponse()
     data = res.read()
     parsedData = json.loads(data)
     print(len(parsedData['results']))
+    
+    for card in parsedData['results']:
+        values = card['stats']['numeric']
+
+        new_card = TriadCard.objects.create(
+            id = card['id'],
+            name = card['name'],
+            stars = card['stars'],
+            icon = card['icon'],
+            image = card['image'],
+            topValue = values['top'],
+            rightValue = values['right'],
+            bottomValue = values['bottom'],
+            leftValue = values['bottom'],
+        )
+        print("Card #{}: \"{}\" added.".format(card['id'], card['name']))
+
     return render(request, "index.html")
