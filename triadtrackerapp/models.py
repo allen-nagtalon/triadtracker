4683f2b184927687970494821af6097e3e4f2b0b
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-09-27 12:43:32 -0700

    Created basic model for TriadCard

--- triadtrackerapp/models.py
+++ triadtrackerapp/models.py
@@ -1,12 +1,43 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, URLValidator
 
 class TriadCard(models.Model):
+    # Primary Key
+    id = models.IntegerField(primary_key=True)
+
+    # Basic Info
     name = models.CharField(max_length=255)
-    stars = models.IntegerField()
-    icon = models.URLField()
-    image = models.URLField()
+    icon = models.URLField(validators=[URLValidator()])
+    image = models.URLField(validators=[URLValidator()])
+    stars = models.IntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
     
-    topValue = models.IntegerField()
-    rightValue = models.IntegerField()
-    bottomValue = models.IntegerField()
-    leftValue = models.IntegerField()
+    # Card Values
+    topValue = models.IntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    rightValue = models.IntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    bottomValue = models.IntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    leftValue = models.IntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
