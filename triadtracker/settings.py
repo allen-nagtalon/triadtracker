73f72c2ea224a5c59bc0c5e6869203dcf7fc4a14
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-10-08 12:23:51 -0700

    Enabled blacklisting tokens in api

--- triadtracker/settings.py
+++ triadtracker/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     
     'triadtrackerapp.apps.TriadtrackerappConfig',
     'loginapp.apps.LoginappConfig',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -161,7 +162,7 @@ CORS_ALLOWED_ORIGINS = [
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
