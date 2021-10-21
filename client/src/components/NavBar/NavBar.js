6cf1ccb04a050592027d2d348eae174b733b3a3f
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-10-21 12:33:16 -0700

    Styled Navbar with proper spacing

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -15,22 +15,41 @@ const NavBar = props => {
     >
       <AppBar position='static'>
         <Toolbar>
-          <Typography component='div' sx={{ flexGrow: 1 }}>
-            <Link href='/' color='inherit' underline='none'>
-              Home
-            </Link>
-          </Typography>
-          <Typography component='div' sx={{ flexGrow: 1 }}>
-            <Link href='/cards' color='inherit' underline='none'>
-              Cards
-            </Link>
-          </Typography>
-          <Typography component='div' sx={{ flexGrow: 1 }}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: 'inline-flex'
+            }}
+          >
+            <Typography
+              component='div'
+              sx={{ mx: 2 }}
+            >
+              <Link href='/' color='inherit' underline='none'>
+                Home
+              </Link>
+            </Typography>
+            <Typography
+              component='div'
+              sx={{ mx: 2 }}
+            >
+              <Link href='/cards' color='inherit' underline='none'>
+                Cards
+              </Link>
+            </Typography>
+          </Box>
+          <Typography
+            component='div'
+            sx={{ mx: 2 }}
+          >
             <Link href='/login' color='inherit' underline='none'>
               Login
             </Link>
           </Typography>
-          <Typography component='div' sx={{ flexGrow: 1 }}>
+          <Typography
+            component='div'
+            sx={{ mx: 2 }}
+          >
             <Link href='/register' color='inherit' underline='none'>
               Register
             </Link>
