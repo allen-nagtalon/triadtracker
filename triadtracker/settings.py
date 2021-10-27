26b365f11c8167b3806c1f584f98e1dc1cb6f279
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-10-27 14:47:30 -0700

    Added dummy static file

--- triadtracker/settings.py
+++ triadtracker/settings.py
@@ -135,8 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = 'loginapp.CustomUser'
 
