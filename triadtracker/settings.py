e43b2c1736692a44e7669182ccafb3006dc92044
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-10-28 13:41:24 -0700

    Removed unnecessary import

--- triadtracker/settings.py
+++ triadtracker/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dotenv
 from pathlib import Path
 from datetime import timedelta
-from boto.s3.connection import S3Connection
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
