2f76d341f1352f36f3193e77e264dd6bbb8e6d4e
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-10-28 18:20:12 -0700

    Added re_path to back-end urls

--- triadtracker/urls.py
+++ triadtracker/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -27,4 +28,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
