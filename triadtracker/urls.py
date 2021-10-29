4c6bba322faf080f08a68fa75053f156797d09a2
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-10-28 18:34:09 -0700

    Added new path for template

--- triadtracker/urls.py
+++ triadtracker/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    re_path('.*',TemplateView.as_view(template_name='index.html'))
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
