864bd7dd4dd8b5b63aaa0a43a3f7ce83aa7c6c14
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-02 15:23:11 -0700

    Allowed CORS to all

--- triadtracker/settings.py
+++ triadtracker/settings.py
@@ -169,9 +169,11 @@ REST_FRAMEWORK = {
     )
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:3000",
+#]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
