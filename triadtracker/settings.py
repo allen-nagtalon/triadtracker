0e2e17a76bb43c0559aceceb553c6938cd31358b
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-02 15:17:50 -0700

    Updated static dir to look in build directory

--- triadtracker/settings.py
+++ triadtracker/settings.py
@@ -136,12 +136,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'build/static'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 
